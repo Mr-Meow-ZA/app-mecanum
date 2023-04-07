03ee8e618da8bb506fb5feed0b9b567c768b58d5
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2023-04-07 12:40:48 +0200

    USed for checking the buttons strings received with dewlimiters

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Yes)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Comma), function () {
+    rx1 = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
+    rx2 = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Comma))
+    temp = 1
+})
+let rx2 = ""
+let rx1 = ""
+let temp = 0
+bluetooth.startUartService()
+basic.showIcon(IconNames.House)
+temp = 0
 basic.forever(function () {
-	
+    if (temp == 1) {
+        serial.writeString("rx1 is ")
+        serial.writeString(rx1)
+        serial.writeLine("- - - ")
+        serial.writeString("rx2 is ")
+        serial.writeString(rx2)
+        serial.writeLine("- - - ")
+        temp = 0
+    } else {
+    	
+    }
 })
